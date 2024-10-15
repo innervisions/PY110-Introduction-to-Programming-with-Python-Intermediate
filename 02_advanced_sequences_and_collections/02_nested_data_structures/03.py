3283dce0d2fde2822087d049b4e17df2c28e6ca1
Raymond Saade <raymond.saade@gmail.com>
2024-10-15 07:46:31 -0400

    completed nested data structures practice problems

---
+++ 02_advanced_sequences_and_collections/02_nested_data_structures/03.py
@@ -0,0 +1,9 @@
+a = 2
+b = [5, 8]
+lst = [a, b]
+
+lst[0] += 2
+lst[1][0] -= a
+
+print(a)  # 2
+print(b)  # [3, 8]
---
+++ 02_advanced_sequences_and_collections/02_nested_data_structures/04.py
@@ -0,0 +1,10 @@
+munsters = {
+    "Herman": {"age": 32, "gender": "male"},
+    "Lily": {"age": 30, "gender": "female"},
+    "Grandpa": {"age": 402, "gender": "male"},
+    "Eddie": {"age": 10, "gender": "male"},
+    "Marilyn": {"age": 23, "gender": "female"},
+}
+
+for person, information in munsters.items():
+    print(f'{person} is a {information['age']}-year-old {information['gender']}.')
