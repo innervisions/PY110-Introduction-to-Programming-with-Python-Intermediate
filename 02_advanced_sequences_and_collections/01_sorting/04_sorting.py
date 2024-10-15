bb3df6c01d118793e192bc1cd4381f3c6826929f
Raymond Saade <raymond.saade@gmail.com>
2024-10-15 07:01:14 -0400

    completed sorting practice  problems

---
+++ 02_advanced_sequences_and_collections/01_sorting/04_sorting.py
@@ -0,0 +1,22 @@
+books = [
+    {
+        'title': 'One Hundred Years of Solitude',
+        'author': 'Gabriel Garcia Marquez',
+        'published': '1967',
+    },
+    {
+        'title': 'The Book of Kells',
+        'author': 'Multiple Authors',
+        'published': '800',
+    },
+    {
+        'title': 'War and Peace',
+        'author': 'Leo Tolstoy',
+        'published': '1869',
+    },
+]
+
+def get_year(book):
+    return int(book['published'])
+
+print(sorted(books, key=get_year))
